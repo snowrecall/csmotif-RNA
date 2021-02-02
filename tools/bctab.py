2290ccbbaba2ef4793c286da41ab2a65e6efb2da
WangYanjiao <wangyanj15@tsinghua.org.cn>
2021-02-02 10:13:04 +0000

    associated moduls

---
+++ tools/bctab.py
@@ -0,0 +1,75 @@
+#/usr/bin/python
+from numpy import *
+################################################################################
+class  BCTab:
+    pass
+#===============================================================================
+    def __init__(self,tab=None):
+        self.CH = {}
+        self.NH = {}
+        self.CHnu = {}
+        self.NHnu = {}
+        self.CHHz = {}
+        self.NHHz = {}
+        self.NH = {}
+        self.N = {}
+        self.H = {}
+        self.C = {}
+        self.num = {}
+        if tab:
+            self.readTab(tab)
+#===============================================================================    
+    def readTab(self,tab):
+        lines = map(str.strip,open(tab).readlines()[1:])
+        Tab1 = {x.split()[0]:float(x.split()[1]) for x in lines }
+        Tab2 = {x.split()[0]:float(x.split()[2]) for x in lines }
+        try:
+            self.num = {x.split()[0]:int(x.split()[3]) for x in lines }
+        except IndexError:
+            pass
+        bds = sorted(Tab1.keys())
+        if all(array(Tab2.values())>9):
+            self.N = {x:Tab1[x] for x in bds}
+            self.H = {x:Tab2[x] for x in bds}
+            self.NH = {x:[Tab1[x],Tab2[x]] for x in bds}
+            self.NHHz = {x:[Tab1[x]*60,Tab2[x]*600] for x in bds}
+            for x in bds:
+                if x[0] in ['G','A']:
+                    self.NHnu[x] = {'N1':Tab1[x],'H1':Tab2[x]} 
+                if x[0] in ['U','C']:
+                    self.NHnu[x] = {'N3':Tab1[x],'H3':Tab2[x]}
+        elif all(array(Tab2.values())<8.6) and all(6.4<array(Tab2.values())) and\
+           all(array(Tab1.values())<148) and all(134<array(Tab1.values())):
+            self.C = {x:Tab1[x] for x in bds}
+            self.H = {x:Tab2[x] for x in bds}
+            self.CH = {x:[Tab1[x],Tab2[x]] for x in bds}
+            self.CHHz = {x:[Tab1[x]*150,Tab2[x]*600] for x in bds}
+            for x in bds:
+                if x[0] in ['G','A']:
+                    self.CHnu[x] = {'C8':Tab1[x],'H8':Tab2[x]} 
+                if x[0] in ['U','C']:
+                    self.CHnu[x] = {'C6':Tab1[x],'H6':Tab2[x]}
+            
+        elif all(array(Tab2.values())<6.4) and all(4.8<array(Tab2.values())) and\
+           all(array(Tab1.values())<98) and all(94<array(Tab1.values())):
+            self.C = {x:Tab1[x] for x in bds}
+            self.H = {x:Tab2[x] for x in bds}
+            self.CH = {x:[Tab1[x],Tab2[x]] for x in bds}
+            self.CHHz = {x:[Tab1[x]*150,Tab2[x]*600] for x in bds}
+            for x in bds:
+                self.CHnu[x] = {"C1'":Tab1[x],"H1'":Tab2[x]}
+        elif all(array(Tab2.values())<8.4) and all(6.0<array(Tab2.values())) and\
+           all(148<array(Tab1.values())) and all(array(Tab1.values())<158):
+            self.C = {x:Tab1[x] for x in bds}
+            self.H = {x:Tab2[x] for x in bds}
+            self.CH = {x:[Tab1[x],Tab2[x]] for x in bds}
+            self.CHHz = {x:[Tab1[x]*150,Tab2[x]*600] for x in bds}
+            for x in bds:
+                self.CHnu[x] = {"C2":Tab1[x],"H2":Tab2[x]}
+
+
+      
+        
+
+            
+
---
+++ tools/fraMotif.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python
+from cstab import *
+from glob import glob
+from common.base import range2list
+
+'''
+Fragment RNA Motif according to secondary structure
+Supported motif types: triplet , penta, base pair
+'''
+
+#===============================================================================
+# find base pairs for each secondary structure
+def pairing(bdstr):
+    stack = []
+    pairs = {}
+
+    for i,nt in enumerate(bdstr):
+        if nt == '(':
+            stack.append(i)
+        elif nt ==')':
+            pairs[stack[-1]] = i
+            pairs[i] = stack[-1]
+            stack[-1:] = []
+    return pairs
+
+#===============================================================================
+
+def getMotif(Mtype,idx,seq,bd):
+    pairs = pairing(bd)
+    if Mtype=='basePair':
+        try:
+            bp_cur = (idx,pairs[idx])
+            bps = [seq[idx]+seq[pairs[idx]]]
+            tps = [x for a in bps for x,y in tp.items() if a in y]
+            mode = bps + tps
+            index = [bp_cur[0],bp_cur[1]]
+            return index,mode
+        except KeyError:
+            return False,False
+
+
+
+    if Mtype=='triplet':
+        try:
+            bp_cur = (idx,pairs[idx])
+            bp_pre = (idx-1,pairs[idx-1])
+            bp_nxt = (idx+1,pairs[idx+1])
+            if bp_cur[1] == bp_pre[1]-1 and bp_cur[1] == bp_nxt[1]+1:
+                bps = [seq[idx]+seq[pairs[idx]],seq[idx-1]+seq[pairs[idx-1]],\
+                seq[idx+1]+seq[pairs[idx+1]]] 
+                tps = [x for a in bps for x,y in tp.items() if a in y]
+                mode = bps + tps
+                index = [bp_cur[0],bp_cur[1],bp_pre[0],bp_pre[1],bp_nxt[0],\
+                bp_nxt[1]]       
+                return index,mode
+            else:
+                return False,False
+        except KeyError:
+            return False,False
+
+
+
+    if Mtype=='penta':
+        try:
+            bp_cur = (idx,pairs[idx])
+            bp_pre = (idx-1,pairs[idx-1])
+            bp_nxt = (idx+1,pairs[idx+1])
+            bp_ppre = (idx-2,pairs[idx-2])
+            bp_nnxt = (idx+2,pairs[idx+2])
+            if bp_cur[1] == bp_pre[1]-1 and bp_cur[1] == bp_nxt[1]+1 and\
+            bp_pre[1] == bp_ppre[1]-1 and bp_nxt[1] == bp_nnxt[1]+1:
+                bps = [seq[idx]+seq[pairs[idx]],seq[idx-1]+seq[pairs[idx-1]],\
+                seq[idx+1]+seq[pairs[idx+1]],seq[idx-2]+seq[pairs[idx-2]],\
+                seq[idx+2]+seq[pairs[idx+2]]] 
+                tps = [x for a in bps for x,y in tp.items() if a in y]
+                mode = bps + tps
+                index = [bp_cur[0],bp_cur[1],bp_pre[0],bp_pre[1],bp_nxt[0],\
+                bp_nxt[1],bp_ppre[0],bp_ppre[1],bp_nnxt[0],bp_nnxt[1]]       
+                return index,mode
+            else:
+                return False,False
+        except KeyError:
+            return False,False
